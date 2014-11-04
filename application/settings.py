9db39523814908864c1a1d5d47fee9de5758be0f
itkr <g600392@gmail.com>
2014-11-04 22:56:45 +0900

    runserverできるように修正

--- application/settings.py
+++ application/settings.py
@@ -102,10 +102,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'autotweet.urls'
+ROOT_URLCONF = 'application.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'autotweet.wsgi.application'
+WSGI_APPLICATION = 'application.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
