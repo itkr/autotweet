b254e416dfe5bd35480dea31c60f2d925a015650
itkr <g600392@gmail.com>
2014-11-07 23:25:06 +0900

    tweet時のcsrf_token追加とTwitterAPIの更新

--- application/module/twitter_api/twitter_api.py
+++ application/module/twitter_api/twitter_api.py
@@ -4,15 +4,12 @@ import twitter
 from social_auth.models import UserSocialAuth
 
 
-class TwitterAPI(object):
+class TwitterAPI(twitter.Api):
 
     def __init__(self, user):
         auth = UserSocialAuth.objects.filter(user=user).get()
-        self.twitter_api = twitter.Api(
+        super(TwitterAPI, self).__init__(
             consumer_key=settings.TWITTER_CONSUMER_KEY,
             consumer_secret=settings.TWITTER_CONSUMER_SECRET,
             access_token_key=auth.tokens["oauth_token"],
             access_token_secret=auth.tokens["oauth_token_secret"])
-
-    def send(self, message):
-        self.twitter_api.PostUpdates(message)
