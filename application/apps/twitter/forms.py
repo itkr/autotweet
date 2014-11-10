ebd8c122b835850491411365c55ac9ff0b2c4018
itkr <g600392@gmail.com>
2014-11-10 23:53:37 +0900

    クライアントを少し変える（仮）

--- application/apps/twitter/forms.py
+++ application/apps/twitter/forms.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from django.forms import Textarea
 
 
 class TweetForm(forms.Form):
-    message = forms.CharField(max_length=140)
+    message = forms.CharField(
+        widget=Textarea(attrs={'cols': 47, 'rows': 3}),
+        max_length=140)
